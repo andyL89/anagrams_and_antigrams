9378da4f92564fa383ac02f2db61835ad27cd9e4
Andy Lawson <alawson89@gmail.com>
2021-02-26 09:26:39 -0800

    troubleshoot anagram vowel spec

--- lib/anagrams_and_antigrams.rb
+++ lib/anagrams_and_antigrams.rb
@@ -1,12 +1,11 @@
 class Words
   def anagram(word1, word2)
-    sorted_word1 = word1.downcase.chars.sort.join
-    sorted_word2 = word2.downcase.chars.sort.join
+    sorted_word1 = word1.downcase.chars.sort.join.any?(/([aeiouy])/g)
+    sorted_word2 = word2.downcase.chars.sort.join.any?(/([aeiouy])/g)
     if sorted_word1.eql?(sorted_word2)
       "These words are anagrams!"
     else
       "These words are NOT anagrams."
     end
   end
-
 end
--- spec/anagrams_and_antigrams_spec.rb
+++ spec/anagrams_and_antigrams_spec.rb
@@ -5,10 +5,14 @@ require('anagrams_and_antigrams')
 describe('Words#anagram') do
   it('Checks to see if 2 words are anagrams of each other') do
     @words = Words.new()
-    expect(@words.anagram('tea', 'eat')).to(eq("These words are anagrams!"))
+    expect(@words.anagram('listen', 'silent')).to(eq("These words are anagrams!"))
   end
   it('Checks to see if 2 words are anagrams of each other despite differing cases') do
     @words = Words.new()
-    expect(@words.anagram('Tea', 'Eat')).to(eq("These words are anagrams!"))
+    expect(@words.anagram('Case', 'Aces')).to(eq("These words are anagrams!"))
+  end
+  it('Checks to see if anagram words both contain vowels') do
+    @words = Words.new()
+    expect(@words.anagram('auctioned', 'cautioned')).to(eq("These words are anagrams!"))
   end
 end
