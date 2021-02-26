6ec299eec0f06e9e462202d98b0174b44d36730b
Andy Lawson <alawson89@gmail.com>
2021-02-26 10:21:41 -0800

    troubleshoot vowel_checker method

--- lib/anagrams_and_antigrams.rb
+++ lib/anagrams_and_antigrams.rb
@@ -1,11 +1,14 @@
 class Words
   def anagram(word1, word2)
-    sorted_word1 = word1.downcase.chars.sort.join
-    sorted_word2 = word2.downcase.chars.sort.join
-    if sorted_word1.eql?(sorted_word2)
+    sorted_1 = word1.downcase.chars.sort.join
+    sorted_2 = word2.downcase.chars.sort.join
+    if sorted_1.eql?(sorted_2)
       "These words are anagrams!"
-    elsif 
+    else
       "These words are NOT anagrams."
+    end
+    def vowel_checker(str)
+      vowels = %w(a e i o u y)
     end
   end
 end
