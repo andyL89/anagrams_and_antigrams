445b122d56b3146315de2a385ac3349708536aeb
Andy Lawson <alawson89@gmail.com>
2021-03-03 13:27:33 -0800

    update script

--- script.rb
+++ script.rb
@@ -3,13 +3,13 @@ require('./lib/anagrams_and_antigrams')
 puts
 puts "Hey there! Wanna test my anagram analyzing skills?"
 sleep 2.5
-loop do
 puts "Enter a word or phrase, and then another."
 sleep 2.5
 puts "If they contain all the same letters, that means they're anagrams!"
 sleep 2.5
 puts "If they have zero matching letters, that means they're antigrams!"
 sleep 2.5
+loop do
 puts "Go ahead, I'm ready!"
 input =  Anagram.new
   input.analyze(gets, gets)
